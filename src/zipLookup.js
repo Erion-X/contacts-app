8a2b2e2c9bd7e2aa1b72f52bd3145bb5d5dbea81
Erion-X <98784085+Erion-X@users.noreply.github.com>
2023-07-01 19:24:38 -0400

    Hardcoded 10 zip codes with City & State information to query first
    App checks pre-loaded zip code data first. If it is not in memory, an API call to USPS is made.

--- src/zipLookup.js
+++ src/zipLookup.js
@@ -1,33 +1,59 @@
 import axios from 'axios';
 
-const USPS_API_URL = 'https://secure.shippingapis.com/ShippingAPI.dll';
-const USER_ID = '2STUDEF595853';
+//Hardcoded Zip Codes w/ City/State Info
+const makeCSObj = (city, state) => ({
+  city,
+  state,
+});
 
+const zipCityState = {
+  10001: makeCSObj('New York', 'NY'),
+  10003: makeCSObj('New York', 'NY'),
+  60601: makeCSObj('Chicago', 'IL'),
+  60602: makeCSObj('Boston', 'MA'),
+  '02113': makeCSObj('Boston', 'MA'),
+  '02114': makeCSObj('Boston', 'MA'),
+  94601: makeCSObj('Oakland', 'CA'),
+  94603: makeCSObj('Oakland', 'CA'),
+  94102: makeCSObj('San Francisco', 'CA'),
+  94103: makeCSObj('San Francisco', 'CA'),
+};
+
+//Corrects All-Caps From API
 const capitalizeFirstLetter = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 };
 
 export default async function lookupCityState(zipCode) {
-  try {
-    const response = await axios.get(USPS_API_URL, {
-      params: {
-        API: 'CityStateLookup',
-        XML: `<CityStateLookupRequest USERID="${USER_ID}"><ZipCode ID="0"><Zip5>${zipCode}</Zip5></ZipCode></CityStateLookupRequest>`,
-      },
-    });
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(response.data, 'text/xml');
-    const city = capitalizeFirstLetter(
-      xmlDoc.getElementsByTagName('City')[0].innerHTML
-    );
-    const state = xmlDoc.getElementsByTagName('State')[0].innerHTML;
+  //Check Pre-loaded Memory for City/State
+  if (zipCityState.hasOwnProperty(zipCode)) {
+    return zipCityState[zipCode];
+  } else {
+    //USPS API Call
+    const USPS_API_URL = 'https://secure.shippingapis.com/ShippingAPI.dll';
+    const USER_ID = '2STUDEF595853';
 
-    return {
-      city,
-      state,
-    };
-  } catch (error) {
-    console.error('Error looking up city and state:', error);
-    throw error;
+    try {
+      const response = await axios.get(USPS_API_URL, {
+        params: {
+          API: 'CityStateLookup',
+          XML: `<CityStateLookupRequest USERID="${USER_ID}"><ZipCode ID="0"><Zip5>${zipCode}</Zip5></ZipCode></CityStateLookupRequest>`,
+        },
+      });
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(response.data, 'text/xml');
+      const city = capitalizeFirstLetter(
+        xmlDoc.getElementsByTagName('City')[0].innerHTML
+      );
+      const state = xmlDoc.getElementsByTagName('State')[0].innerHTML;
+
+      return {
+        city,
+        state,
+      };
+    } catch (error) {
+      console.error('Error looking up city and state:', error);
+      throw error;
+    }
   }
 }
